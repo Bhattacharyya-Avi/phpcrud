58148774df4476585525db5603a181bae3e2a82f
Bhattacharyya-Avi <ab.kodeeo@gmail.com>
2022-05-03 19:44:30 +0000

    update

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-hello
+<!-- hello -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+    <title>PHP crude</title>
+</head>
+<body>
+    <div class="container">
+        <p>Basic CRUD oparation using php</p>
+        <div class="my-5">
+            <button class="btn btn-info"><a style="color: white;" href="user_list.php">Let's Start</a></button>
+        </div>
+    </div>
+</body>
+</html>
